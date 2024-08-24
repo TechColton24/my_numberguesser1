a1cba2f4c05a656b5fde4e3e0c89149ea3c2e9f5
Colton Hugh Linde <tech.colton24@gmail.com>
2024-08-24 21:27:14 +0100

    Number Guesser
    This a number guessing generator that I made to practice 'while loops and if statements'

---
+++ number_guesser.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1, 100)
+guess = 0
+
+while guess != number:
+  guess = int(input("Enter a guess: "))
+  
+  if (guess < number):
+    print("Guess Higher!")
+    
+  elif (guess > number):
+    print("Guess Lower!")
+    
+  else:
+    print("You Won!")
